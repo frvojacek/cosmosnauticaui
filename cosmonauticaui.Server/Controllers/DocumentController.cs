1074d023f46d621eb6f369f799b6026f5d1421b9
František Vojáček <fr.vojacek@email.cz>
2024-08-09 12:43:11 +0200

    feat: add document POST API endpoint

---
+++ cosmonauticaui.Server/Controllers/DocumentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cosmonauticaui.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DocumentController : ControllerBase
+	{
+		[HttpPost]
+		[Consumes("multipart/form-data")]
+		public IActionResult Post(IFormFile file)
+		{
+			return Ok(file);
+		}
+	}
+}

568eaad7c2e43c915e934231dd32731d415631dc
Jakub Matějíček <jakub6matejcek@gmail.com>
2024-08-23 09:17:35 +0200

    fix: icons

--- cosmonauticaui.client/src/App.tsx
+++ cosmonauticaui.client/src/App.tsx
@@ -95,10 +95,10 @@ function App() {
             <td>{document.products.join(", ")}</td>
             <td className="td_icons">
                 <a href={`${domain}/api/Document/${document.id}`}>
-                    <button><img src="/src/SVGs/download_icon.svg" alt="Download" /></button>
+                    <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#002e3c"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" /></svg></button>
                 </a>
-                <button onClick={() => handleEditDocument(document)}><img src="/src/SVGs/edit_icon.svg" alt="Edit" /></button>
-                <button onClick={() => handleDelete(document.id)}><img src="/src/SVGs/delete_icon.svg" alt="Delete" /></button>
+                <button onClick={() => handleEditDocument(document)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#002e3c"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" /></svg></button>
+                <button onClick={() => handleDelete(document.id)}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#002e3c"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" /></svg></button>
             </td>
         </tr>
     ));
@@ -106,7 +106,7 @@ function App() {
     return (
         <>
             <header>
-                <img className="logo" src="src/SVGs/CommerzBank.svg" aria-label="Commerzbank Logo"></img>
+                <img className="logo" src="https://www.commerzbank.de/ms/media/favicons/CB-2022-Ribbon_RGB.svg" aria-label="Commerzbank Logo"></img>
                 <h1>DRIFT</h1>
             </header>
             <div className="content">
@@ -119,7 +119,7 @@ function App() {
                         <form id="document-create" onSubmit={handleSubmit}>
                             <div className="document-create-header">
                                 <h2>{selectedDocument ? "Edit document" : "New document"}</h2>
-                                <button id="closeButton" type="button" onClick={() => { createDialog.current?.close(); setSelectedDocument(null); }}><img src="src/SVGs/close_icon.svg" alt="Close" /></button>
+                                <button id="closeButton" type="button" onClick={() => { createDialog.current?.close(); setSelectedDocument(null); }}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></button>
                             </div>
                             <div className="metadata">
                                 <label htmlFor="Name">Name</label>
@@ -155,7 +155,7 @@ function App() {
                     <option value="products">Product</option>
                 </select>
                 <input id="searchInput" name="searchInput" type="text"></input>
-                <button><img src="/src/SVGs/search_icon.svg"></img></button>
+                            <button><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#002e3c"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg></button>
             </form>
             <table>
                 <thead>
